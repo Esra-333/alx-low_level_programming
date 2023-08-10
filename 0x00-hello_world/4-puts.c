37e6847f418133b06e9c23e7d81bb11d290e1ebe
Esra-333 <esra.omer444@gmail.com>
2023-08-09 23:42:44 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * main - print ("Programming is like building a multilingual puzzle
  * Return:0
  */
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	puts "Programming is like building a multilingual puzzle";
+	Return (0)
 }
