0fea23b6b4daa329e878538f140ecac4a896b6a5
Esra-333 <esra.omer444@gmail.com>
2023-08-10 00:21:59 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - prints  with proper grammar,
- *   * but the outcome is a piece of art,
- *    * Return always:0
- */    
+ * main - prints with proper grammar
+ * but the outcome is a piece of art,
+ * Return:Always 0.
+ */
+ 
+     
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+		return (0);
 }
