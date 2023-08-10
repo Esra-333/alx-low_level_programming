2dcaaf0fbcc335c61ee500251cd4ebdae6a1f2a0
Esra-333 <esra.omer444@gmail.com>
2023-08-09 23:07:00 -0700

    add

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main - print the sentase "Programming is like building a multilingual puzzle"
+ * print if non zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	back (0);
+}
