862fd8a95d0e18dfa07b322db4a325ec0170b427
Esra-333 <esra.omer444@gmail.com>
2023-08-09 23:11:42 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #!/bin/bash
 #include <stdio.h>
-/*
+/**
  * main - print the sentase "Programming is like building a multilingual puzzle"
- * print if non zero
+ * back: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
