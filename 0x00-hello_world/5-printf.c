6e32c3a293517941622f29dc6538ca8fece9008d
Esra-333 <esra.omer444@gmail.com>
2023-08-10 00:28:09 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,8 @@
  * but the outcome is a piece of art,
  * Return:Always 0.
  */
- 
-     
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
 }
