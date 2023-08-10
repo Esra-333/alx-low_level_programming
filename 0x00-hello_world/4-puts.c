1cdd81bb6af9075f6b33ae111f34062d0f62baeb
Esra-333 <esra.omer444@gmail.com>
2023-08-09 23:59:15 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints "Programming is like building a multilingual puzzle.
  * * Return: Always 0.
  */
-int main(void){
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
