45bc395b4e2de7e829134b1e6cf584c960e5c6b1
Esra-333 <esra.omer444@gmail.com>
2023-08-11 11:33:13 -0700

    add

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,15 +13,15 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+	        	putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
 				{
-					putchar(',');
-					putchar(' ');
+			           putchar(',');
+				   putchar(' ');
 				}
 			}
 		}
