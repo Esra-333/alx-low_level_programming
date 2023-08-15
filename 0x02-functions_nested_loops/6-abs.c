293ff72d8f50b7e0c12f37ed9d8cfe3a16b77874
Esra-333 <esra.omer444@gmail.com>
2023-08-15 11:09:21 -0700

    a

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - check the code
- * computes the absolute value of an integer.
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check,
  * Return: the absolute value of int
  */
 int _abs(int n)
