3515c400cd9882dbece4753068791cb0e9e8f36c
Esra-333 <esra.omer444@gmail.com>
2023-08-15 04:34:35 -0700

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints _putchar, followed by a new line,
  * Return: Always 0 (Success)
@@ -8,9 +7,9 @@ int main(void)
 {
 	char str[] = _putchar;
 	int ch;
- for (ch =0; ch < 8; ch++)
-      _putchar(str[ch]);
-       _putchar("\n");
+  for (ch = 0; ch < 8; ch++)
+   _putchar(str[ch]);
+   _putchar("\n");
 
   return (0);
 }
