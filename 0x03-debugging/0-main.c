066736a15d798a4585cf692324751a8d3e98fc96
Esra-333 <esra.omer444@gmail.com>
2023-08-15 15:57:42 -0700

    a

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test fuction can print if integer is posstive or negative,
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
