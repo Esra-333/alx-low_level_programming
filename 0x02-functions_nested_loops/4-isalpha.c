80001654001f92f4980c22b92a2495cf435bfaa8
Esra-333 <esra.omer444@gmail.com>
2023-08-15 10:53:05 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * 4-isalpha.c,
+ * @c: the character to be checked,,
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
