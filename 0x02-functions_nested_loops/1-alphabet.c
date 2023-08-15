4bc8117078bd9200edaf75f49b69a721c39dfa01
Esra-333 <esra.omer444@gmail.com>
2023-08-15 09:16:57 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - utilizes on the _putchar function to print the alphabet a -z 
+ *print_alphabet - utilizes on the _putchar function to print the alphabet a -z. 
  */
 void print_alphabet(void)
 {
