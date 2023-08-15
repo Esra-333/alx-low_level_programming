d8fb0a0c156a576a0847d78f90d7d8a1e247fdf5
Esra-333 <esra.omer444@gmail.com>
2023-08-15 06:40:27 -0700

    add

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,8 +2,12 @@
 #include "main.h"
 
 /**
- * main - check code
- * return on success 1
+ * _putchar - write the character c to stout
+ * @c : the character to print,
+ *
+ * return : on success 1.
+ *    on error. -1 is returned . and errno is set approgriately,
+ *    Return: Always 0.
  */
 int _putchar(char c)
 {
