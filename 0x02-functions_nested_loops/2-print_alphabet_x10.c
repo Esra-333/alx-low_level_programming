de8ef15c9cf5ab812af2b3003346716e9e7bdc5f
Esra-333 <esra.omer444@gmail.com>
2023-08-14 23:15:46 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * prints 10 times the alphabet, in lowercase,
+ * followed by a new line,
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i=0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
