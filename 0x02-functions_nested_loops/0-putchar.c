807d54300219b82e402b4667b82fa651e8095d79
Esra-333 <esra.omer444@gmail.com>
2023-08-15 07:49:14 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,11 @@
 
 int main(void)
 {
-	char str[] = "_putchar";
+	char str[] = '_putchar';
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
 		_putchar (str[ch]);
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
