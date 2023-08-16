6bb974b7bc60bf01cefc180cec25650b98a6fb15
Esra-333 <esra.omer444@gmail.com>
2023-08-16 03:50:00 -0700

    q

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - test fuction can print if integer is posstive or negative,
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
-	int x;
+	int i;
 
 	x = 98 % 2;
-	positive_or_negative(x);
+	positive_or_negative(i);
 
 	return (0);
 }
