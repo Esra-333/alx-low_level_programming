78adc9439a228900af741488364b008940db0226
Esra-333 <esra.omer444@gmail.com>
2023-08-17 03:51:25 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * main - prints the numbers from 1 to 100, followed by new line
  * but for multiples of three prints Fizz instead of number
