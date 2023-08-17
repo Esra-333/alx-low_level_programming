fc8e95ea38a828ceb90252eb46c68ba840869b5b
Esra-333 <esra.omer444@gmail.com>
2023-08-17 11:10:36 -0700

    a

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if 
+ * integer is positive or negative
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
