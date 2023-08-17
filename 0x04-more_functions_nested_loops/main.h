fb35a2ca2f28b2fa28fd6b6392ee73943a9f0309
Esra-333 <esra.omer444@gmail.com>
2023-08-16 23:31:28 -0700

    a

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,7 +8,8 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 void print_most_numbers(void);
-
+void more_numbers(void)i
 
 (#endif
