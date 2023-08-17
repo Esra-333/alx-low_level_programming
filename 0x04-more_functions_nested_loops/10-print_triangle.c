bcb29cd9feab7dbefc8c5ef66f2b05a7831fc8e1
Esra-333 <esra.omer444@gmail.com>
2023-08-17 04:20:50 -0700

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,7 +5,7 @@
  */
 void print_triangle(int size)
 {
-	if (size < 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
@@ -18,6 +18,11 @@ void print_triangle(int size)
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
