10adf01c1dea7965651b96475d18ccb70c3c298a
Esra-333 <esra.omer444@gmail.com>
2023-08-23 21:44:59 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,28 +27,29 @@ else
 putchar('.');
 }
 }
-			/**
-			 * print_buffer - prints a buffer
-			 * @b: buffer to print
-			 * @size: size of buffer
-			 * Return: void
-			 */
-			void print_buffer(char *b, int size)
-			{
-			int i;
-			for (i = 0; i <= (size - 1) / 10 && size; i++)
-			{
-			printf("%08x: ", i * 10);
-			if (i < size / 10)
-			{
-			print_line(b, 9, i);
-			}
-			else
-			{
-			print_line(b, size % 10 - 1, i);
-			}
-			putchar('\n');
-			}
-			if (size == 0)
-				putchar('\n');
-			}
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+int i;
+			
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
+}
