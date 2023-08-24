9c1da73973b7c22c6063b9d5113d6cdb2ce6a92c
Esra-333 <esra.omer444@gmail.com>
2023-08-23 22:02:00 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "maun.h"
 #include <stdio.h>
 /**
  * print_line - prints a s bytes of a buffer
@@ -10,22 +10,22 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
-
 	for (j = 0; j <= 9; j++)
-	if (j <= s)
-	printf("%02x", c[1 * 10 + j]);
-	else
-	printf("  ");
-	if (j % 2)
-	putchar(' ');
-}
-for (k = 0; k <= s; k++)
-{
-if (c[1 * 10 + k] > 31 && c[1 * 10 + k]
-putchar(c[1 * 10 + k]);
-else
-putchar('.');
-}
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
 }
 /**
  * print_buffer - prints a buffer
@@ -35,21 +35,21 @@ putchar('.');
  */
 void print_buffer(char *b, int size)
 {
-int i;
-			
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-{
-printf("%08x: ", i * 10);
-if (i < size / 10)
-{
-print_line(b, 9, i);
-}
-else
-{
-print_line(b, size % 10 - 1, i);
-}
-putchar('\n');
-}
-if (size == 0)
-putchar('\n');
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
