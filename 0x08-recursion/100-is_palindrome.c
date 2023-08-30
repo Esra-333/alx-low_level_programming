38572c5dc5c68da9b50b1bd60dcbc9cfe08fd6de
Esra-333 <esra.omer444@gmail.com>
2023-08-30 00:37:43 -0700

    a

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,6 +6,17 @@ int _strlen_recursion(char *s);
  * @s: string to reverse
  * Return: 1 if it is, 0 it's not
  */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ * Return: length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
