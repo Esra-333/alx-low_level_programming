a1ae533bd50ac951a6926ad8280e953724e28773
Esra-333 <esra.omer444@gmail.com>
2023-09-04 00:30:48 -0700

    a

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,107 @@
+ *
+ *  */
+ void times_table(void);
+ /**
+  *  * add - adds stuff up
+  *   *
+  *    * @int: integers to add
+  *     * Return: returns a sum
+  *      */
+ int add(int, int);
+ /**
+  *  * print_to_98 - prints to 98
+  *   * @n: integer to start at
+  *    *
+  *     */
+ void print_to_98(int n);
+ /**
+  *  * print_times_table - whatever man
+  *   *@n: integer to put in this table
+  *    *
+  *     */
+ void print_times_table(int n);
+ /**
+  *  * _isupper - determines uppercase or not
+  *   * @c: character to determine
+  *    * Return: true or false
+  *     */
+ int _isupper(int c);
+ /**
+  *  * _isdigit - checks if it is digit
+  *   * @c: character to check
+  *    *
+  *     * Return: returns boolean
+  *      */
+ int _isdigit(int c);
+ /**
+  *  * mul - multiplies stuff
+  *   * @a: integer to multiply
+  *    * @b: integer to multiply
+  *     * Return: gives result
+  *      */
+ int mul(int a, int b);
+ /**
+  *  * print_numbers - prints the numbers
+  *   *
+  *    */
+ void print_numbers(void);
+ /**
+  *  * print_most_numbers - prints the numbers
+  *   *
+  *    */
+ void print_most_numbers(void);
+
+ /**
+  *  * more_numbers - prints the numbers
+  *   *
+  *    */
+ void more_numbers(void);
+ /**
+  *  * print_line - prints a line
+  *   *
+  *    * @n: integer to print stuff
+  *     */
+ void print_line(int n);
+ /**
+  *  * print_diagonal - prints diagonal
+  *   *
+  *    * @n: integer to see how many stupid lines
+  *     */
+ void print_diagonal(int n);
+ /**
+  *  * print_square - prints a square
+  *   *
+  *    * @size: prints the size of whatever
+  *     */
+ void print_square(int size);
+ /**
+  *  * print_triangle - prints triangle
+  *   * @size: size of triangle
+  *    */
+ void print_triangle(int size);
+ /**
+  *  * print_number - prints number
+  *   *
+  *    * @n: integer to print to char
+  *     */
+ void print_number(int n);
+ /**
+  *  * reset_to_98 - resets variable to 98
+  *   *
+  *    * @n: variable with pointer
+  *     */
+ void reset_to_98(int *n);
+ /**
+  *  * swap_int - swapts integer values
+  *   * @a: first integer
+  *    * @b: second integer
+  *     */
+ void swap_int(int *a, int *b);
+ /**
+  *  * _strlen - length of string
+  *   * @s: string
+  *    *
+  *     * Return: returns length
+  *      */
+ int _strlen(char *s);
+#endif
