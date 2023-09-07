961157f523795bb73ec3be231c7db9deb19e30dc
Esra-333 <esra.omer444@gmail.com>
2023-09-07 04:18:45 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-		len++;
+		len1++;
 	while (s2 && s2[len2])
-		len++;
+		len2++;
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
