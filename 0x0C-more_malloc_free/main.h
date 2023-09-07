79b05376de4a525455935f87cd86530ed98837b9
Esra-333 <esra.omer444@gmail.com>
2023-09-06 23:29:26 -0700

    a

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,7 +10,7 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
-
+int is_digit(char *s);
 
 
 
