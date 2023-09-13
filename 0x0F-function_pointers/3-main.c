8878b874ebbc93300be4dd34c7a2824b8391fcd8
Esra-333 <esra.omer444@gmail.com>
2023-09-13 06:45:14 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,15 +16,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
+
 	if (!func)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
+
 	o = *argv[2];
 
 	if ((o == '/' || o == '%') && arg2 == 0)
