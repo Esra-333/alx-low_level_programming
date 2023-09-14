e0aea1ba6b7ca5dcccd2b7b9ec8f1834c9e3ea1b
Esra-333 <esra.omer444@gmail.com>
2023-09-14 04:00:22 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,7 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - calculates the sum of all its parameters
