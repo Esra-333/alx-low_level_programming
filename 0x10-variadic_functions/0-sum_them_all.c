78d77525304c5e3cc8d46a9a407668d02b3b7ba2
Esra-333 <esra.omer444@gmail.com>
2023-09-14 06:46:54 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,13 +15,17 @@ int sum_them_all(const unsigned int n, ...)
 	va_list argSum;
 
 	if (n == 0)
+	{
 		return (0);
+	}
 
 	va_start(argSum, n);
 	sum = 0;
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(argSum, int);
+	}
 
 	va_end(argSum);
 
