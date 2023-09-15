36d27151cdca804d601f532b48c609e4f29434ce
Esra-333 <esra.omer444@gmail.com>
2023-09-14 17:24:04 -0700

    a

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceis", 'H', 0, "lberton");
+	print_all(NULL);
+	return (0);
+}
