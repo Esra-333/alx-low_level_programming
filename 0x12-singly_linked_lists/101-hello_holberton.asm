da046f9aefd41c6440ef63e79913cfe5725834c7
Esra-333 <esra.omer444@gmail.com>
2023-09-21 04:09:53 -0700

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,7 @@
            global      main
            extern      printf
    main:
-           mov     edit, format
+           mov     edi, format
            xor     eax, eax
            call    printf
            mov     eax, 0
