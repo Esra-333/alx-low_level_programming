84b04984fff5bfc46643c504ba4ae8bf504d9add
Esra-333 <esra.omer444@gmail.com>
2023-10-26 09:35:24 -0700

    a

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * get_endianness - find type of endianness
- * Returns: 0 if big endian, 1 if little endian
+ * get_endianness - check endianness
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	unsigned int y = 1;
+	unsigned int y;
+
+	y = 1;
+
 	char *c = (char *)&y;
 
 	if (*c)
