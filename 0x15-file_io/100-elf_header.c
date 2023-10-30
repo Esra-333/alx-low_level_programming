1f4e7a3d84c0dd57d5a5f8174f5039411131ea25
Esra-333 <esra.omer444@gmail.com>
2023-10-30 12:34:11 -0700

    a

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,10 @@
 #include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
