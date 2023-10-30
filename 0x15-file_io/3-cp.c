aed6c475aadddecbdda95bdb781ac6152af0a58c
Esra-333 <esra.omer444@gmail.com>
2023-10-30 09:11:41 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
-#define BUFFERSIZE 1204
+#include <stdlib.h>
+
+#define BUFFERSIZE 1024
 /**
  * main - Copies content
  * @argc: Arguments count.
@@ -19,6 +20,9 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_in = open(argv[1], O_RDONLY);
+
+	fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode_file);
+
 
 	if (fd_out == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
