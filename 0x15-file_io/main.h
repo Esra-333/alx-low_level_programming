1208ed80d0a63667b6ff7994145df9ee27824ba7
Esra-333 <esra.omer444@gmail.com>
2023-10-30 04:37:23 -0700

    a

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,5 +16,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int finsh_function(int num, char *str);
 
 #endif
