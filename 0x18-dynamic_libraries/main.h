e81a86850452fd7422b461c823e0cba295526b21
Esra-333 <esra.omer444@gmail.com>
2023-12-11 01:25:17 -0800

    a

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef DYNAMIC_LIB
+#define DYNAMIC_LIB
 #include <stdlib.h>
-#include <stdio.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,7 +8,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -24,4 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+#endif /* DYNAMIC_LIB */
