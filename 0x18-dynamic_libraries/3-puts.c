74bd8ab31d6d4061e80449aa59e57f4d4b4e57d1
Esra-333 <esra.omer444@gmail.com>
2023-12-11 02:49:58 -0800

    a

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		 str++;
+	}
+	_putchar('\n');
+}
