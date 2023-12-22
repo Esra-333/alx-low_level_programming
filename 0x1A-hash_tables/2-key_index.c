8edc6a03967db7c10c15649a08334838f2d498ce
Esra-333 <esra.omer444@gmail.com>
2023-12-21 23:02:02 -0800

    a

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  * Return: The index of the key.
  * Description: Uses the djb2 algorithm.
  */
-unsigned long int key_index(const unsigned char *key, unsigned long)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
