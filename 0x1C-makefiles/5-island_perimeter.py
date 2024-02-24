0468d651ab599bf490395790643e3083b6013332
Esra-333 <esra.omer444@gmail.com>
2024-02-23 22:58:42 -0800

    a

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,4 +22,4 @@ def island_perimeter(grid):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
-        return size * 4 - edges * 2
+    return size * 4 - edges * 2
