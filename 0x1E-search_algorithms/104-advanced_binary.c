c175f6c0c9ad91b74f023a5e8011ff02863f225b
Esra-333 <esra.omer444@gmail.com>
2024-05-07 23:00:05 -0700

    a

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
  * rec_search - searches for a value in an array of
  * @array: input array
@@ -17,26 +18,22 @@ int rec_search(int *array, size_t size, int value)
 	printf("Searching in array");
 
 	for (i = 0; i < size; i++)
-	{
 		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-		printf("\n");
-
-		if (half && size % 2 == 0)
-			half--;
-
-		if (value == array[half])
-		{
-			if (half > 0)
-				return (rec_search(array, half + 1, value));
-			return ((int)half);
-		}
-
-		if (value < array[half])
+	printf("\n");
+	
+	if (half && size % 2 == 0)
+		half--;
+	if (value == array[half])
+	{
+		if (half > 0)
 			return (rec_search(array, half + 1, value));
-
-		half++;
-		return (rec_search(array + half, size - half, value) + half);
+		return ((int)half);
 	}
+	
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
 }
 /**
  * advanced_binary - calls to rec_search to return
